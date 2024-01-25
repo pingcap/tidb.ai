f3719ce6267fe690c2aabbbe7638a0dd8ae87936
Jagger <634750802@qq.com>
2024-01-26 00:10:55 +0800

    update

--- src/core/db/db.ts
+++ src/core/db/db.ts
@@ -5,7 +5,10 @@ import { createPool } from 'mysql2';
 export const db = new Kysely<DB>({
   dialect: new MysqlDialect({
     pool: createPool({
-      uri: process.env.DATABASE_URL!
+      uri: process.env.DATABASE_URL!,
+      ssl: {
+        minVersion: 'TLSv1.2',
+      }
     })
   })
 })
