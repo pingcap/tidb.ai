f8d0207edd7fd12c77fd95a12e3fc6e597c46de8
Jagger <634750802@qq.com>
2024-01-26 18:22:19 +0800

    fix

--- src/core/db/db.ts
+++ src/core/db/db.ts
@@ -6,7 +6,7 @@ export const db = new Kysely<DB>({
   dialect: new MysqlDialect({
     pool: createPool({
       uri: process.env.DATABASE_URL!,
-      ssl: process.env.NODE_ENV?.includes('tidbcloud.com') ? {
+      ssl: process.env.DATABASE_URL?.includes('tidbcloud.com') ? {
         minVersion: 'TLSv1.2',
       } : undefined,
     }),
