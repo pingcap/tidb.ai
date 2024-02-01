8fde16385f7414dd9f068a347c555468f1e7c75b
Jagger <634750802@qq.com>
2024-02-01 15:42:48 +0800

    fix

--- src/app/(main)/docs/(pure)/layout.tsx
+++ src/app/(main)/docs/(pure)/layout.tsx
@@ -1,6 +1,6 @@
 import type { ReactNode } from 'react';
 
-export default function ({ children }: { children: ReactNode }) {
+export default function Layout ({ children }: { children: ReactNode }) {
   return (
     <div className='p-4'>
       <article className='prose prose-sm prose-neutral dark:prose-invert overflow-x-hidden break-all'>
