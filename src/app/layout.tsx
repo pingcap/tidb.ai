c86042907999b732ced8fec061e7451898d80a98
czhen <56986964+shczhen@users.noreply.github.com>
2024-02-01 21:19:08 +0800

    update ppt modal
    update js url

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,7 +25,7 @@ export default async function RootLayout ({
     <html lang="en" suppressHydrationWarning>
     <head>
       <link rel="icon" href="/tidb-ai-light.svg" sizes="any" />
-      <script async src="https://conversation-search-box-git-test-cookie-shczhen-pingcap.vercel.app/assets/index-KaY9SOi_.js" />
+      <script async src="https://conversation-search-box.vercel.app/assets/index-gZ_krR4M.js" />
     </head>
     <body className={font.className}>
     <ThemeProvider
