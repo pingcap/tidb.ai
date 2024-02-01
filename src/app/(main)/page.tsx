2e615d268713cba0ff2e2004a978a7ddd91bfc4f
Jagger <634750802@qq.com>
2024-02-01 19:39:14 +0800

    just ppt

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -4,12 +4,13 @@ import { Ask } from '@/components/ask';
 import { Highlight } from '@/components/highlight';
 import { Button } from '@/components/ui/button';
 import { useAsk } from '@/components/use-ask';
+import { CopyIcon } from 'lucide-react';
 
 export default function Page () {
   const { loading, ask } = useAsk();
 
   return (
-    <div className="h-body md:h-screen flex flex-col items-center justify-center gap-4">
+    <div className="h-body md:h-screen flex flex-col items-center justify-center gap-4 relative">
       <h1 className="text-4xl font-semibold">
         <Highlight>Scenario 1:</Highlight> Conversational Ask
       </h1>
@@ -25,6 +26,19 @@ export default function Page () {
           </li>
         ))}
       </ul>
+      <div className="absolute bottom-4 left-1/2 -translate-x-1/2 text-sm text-center space-y-2">
+        <div>
+          One command to get same for TiDB Cloud users.
+        </div>
+        <div className="flex gap-2 py-2 px-4 rounded-full bg-primary/70 text-primary-foreground items-center">
+          <code className="block">
+            <pre><span className='opacity-70 select-none'>$ </span>ticloud create-app --template rag --bind 8080</pre>
+          </code>
+          <Button className='w-6 h-6 rounded-full' size='icon' variant='ghost'>
+            <CopyIcon size='1em' />
+          </Button>
+        </div>
+      </div>
     </div>
   );
 }
