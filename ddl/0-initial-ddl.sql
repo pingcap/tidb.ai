992892c88049a586467e6c0937043f518d58238f
Jagger <634750802@qq.com>
2024-02-05 17:44:05 +0800

    update

--- ddl/0-initial-ddl.sql
+++ ddl/0-initial-ddl.sql
@@ -206,5 +206,20 @@ SET config = JSON_MERGE_PATCH(config, '{
     "top_k": 5,
     "template": "Use the following pieces of context to answer the user question. This context retrieved from a knowledge base and you should use only the facts from the context to answer.\\nYour answer must be based on the context. If the context not contain the answer, just say that ''I don''t know'', don''t try to make up an answer, use the context.\\n\\n<contexts>\\n{%- for context in contexts %}\\n  <context source_uri=\\"{{context.source_uri}}\\" name=\\"{{context.source_name}}\\">\\n    <name>{{context.source_name}}</name>\\n    <source_uri>{{context.source_uri}}</source_uri>\\n    <content>{{context.text_content}}</content>\\n  </context>\\n{%- endfor %}\\n</contexts>\\n\\nYour answer must be based on the context, don''t use your own knowledge. \\n\\nUse markdown to answer. Write down uri reference you used for answer the question.\\n"
   }
-}')
+}');
+
+UPDATE `index`
+SET config = JSON_MERGE(config, '{
+  "rag.loader.html": {
+    "contentExtraction": {
+      "docs.pingcap.com": [
+        {
+          "pattern": "/**",
+          "contentSelector": ".doc-content"
+        }
+      ]
+    }
+  }
+}'),
+    last_modified_at = CURRENT_TIMESTAMP
 WHERE name = 'default';
