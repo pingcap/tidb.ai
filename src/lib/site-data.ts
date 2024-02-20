5d87702c92e4a324ada553e90e75cdf01d878cdf
Jagger <634750802@qq.com>
2024-02-20 19:38:37 +0800

    update search params

--- src/lib/site-data.ts
+++ src/lib/site-data.ts
@@ -2,5 +2,6 @@ export const searching_uri_prefix = [
   'https://docs.pingcap.com/tidbcloud/',
   'https://docs.pingcap.com/tidb/stable/',
   'https://ask.pingcap.com/t/',
-  'https://www.pingcap.com/blog/'
+  'https://www.pingcap.com/blog/',
+  'https://www.pingcap.com/case-study/'
 ];
