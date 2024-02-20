503457fc4d4c2fcef37fc75f8b28ba4c6c9607e6
sykp241095 <sykp241095@gmail.com>
2024-02-20 10:44:40 +0800

    chore: add a meaningful title

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -11,7 +11,7 @@ export default function Page () {
   return (
     <div className="h-body md:h-screen flex flex-col items-center justify-center gap-4 relative">
       <h1 className="text-4xl font-semibold">
-        Conversational Ask
+        Ask anything about TiDB & TiDB Cloud
       </h1>
       <Ask className="px-4 w-full lg:w-2/3" loading={loading} ask={ask} />
       <ul className="flex gap-2 flex-wrap px-4 w-full lg:w-2/3">
@@ -25,19 +25,6 @@ export default function Page () {
           </li>
         ))}
       </ul>
-      <div className="absolute bottom-4 left-1/2 -translate-x-1/2 text-sm text-center space-y-2">
-        <div>
-          One command to get same for TiDB Cloud users.
-        </div>
-        <div className="flex gap-2 py-2 px-4 rounded-full bg-primary/70 text-primary-foreground items-center">
-          <code className="block">
-            <pre><span className="opacity-70 select-none">$ </span>ticloud create-app --template rag --bind 8080</pre>
-          </code>
-          <Button className="w-6 h-6 rounded-full" size="icon" variant="ghost">
-            <CopyIcon size="1em" />
-          </Button>
-        </div>
-      </div>
     </div>
   );
 }
