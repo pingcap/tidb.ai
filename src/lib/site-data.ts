249040e246c938dbbf6c28e5efca19cb95f3e3cb
Jagger <634750802@qq.com>
2024-02-28 19:45:30 +0800

    update search prefix

--- src/lib/site-data.ts
+++ src/lib/site-data.ts
@@ -3,5 +3,6 @@ export const searching_uri_prefix = [
   'https://docs.pingcap.com/tidb/stable/',
   'https://ask.pingcap.com/t/',
   'https://www.pingcap.com/blog/',
-  'https://www.pingcap.com/case-study/'
+  'https://www.pingcap.com/case-study/',
+  'https://asktug.com/t/',
 ];
