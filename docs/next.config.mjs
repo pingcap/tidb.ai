80be62335cdacc1b0fddb83f91ad5771c179f06a
czhen <56986964+shczhen@users.noreply.github.com>
2024-03-04 13:52:02 +0800

    [doc] init

---
+++ docs/next.config.mjs
@@ -0,0 +1,13 @@
+import nextra from 'nextra';
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+};
+
+const withNextra = nextra({
+  theme: 'nextra-theme-docs',
+  themeConfig: './theme.config.tsx',
+});
+
+export default withNextra;
