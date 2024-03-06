bacffd4351ec152c12d65d8931e98bb75c6f3135
czhen <56986964+shczhen@users.noreply.github.com>
2024-03-06 17:53:04 +0800

    feat: support custom static values
    https://github.com/pingcap/tidb.ai/issues/32

--- src/app/(main)/nav.tsx
+++ src/app/(main)/nav.tsx
@@ -2,7 +2,7 @@
 
 import { Ask } from '@/components/ask';
 import { type NavGroup, type NavItem, SiteNav } from '@/components/site-nav';
-import { SiteNavFooter } from '@/components/site-nav-footer';
+import { SiteNavFooter, type SiteSocialsType } from '@/components/site-nav-footer';
 import { Button } from '@/components/ui/button';
 import { Dialog, DialogOverlay, DialogPortal } from '@/components/ui/dialog';
 import { Drawer, DrawerContent, DrawerTrigger } from '@/components/ui/drawer';
@@ -108,7 +108,9 @@ export function Nav () {
   );
 }
 
-export function NavDrawer () {
+export function NavDrawer(props: {
+  social?: SiteSocialsType;
+}) {
   return (
     <Drawer>
       <DrawerTrigger className="flex md:hidden flex-shrink-0" asChild>
@@ -119,7 +121,7 @@ export function NavDrawer () {
       <DrawerContent className="px-2">
         <ScrollArea className="h-[50vh]">
           <Nav />
-          <SiteNavFooter className="bg-background" />
+          <SiteNavFooter className="bg-background" social={props?.social} />
         </ScrollArea>
       </DrawerContent>
     </Drawer>
