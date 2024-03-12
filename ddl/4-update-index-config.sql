7da4e399134b430c56c4b7fca2f663b54a8974ba
Jagger <634750802@qq.com>
2024-03-12 13:59:36 +0800

    fix

--- ddl/4-update-index-config.sql
+++ ddl/4-update-index-config.sql
@@ -1,6 +1,6 @@
 ALTER TABLE `index`
     ADD COLUMN embedding VARCHAR(32) NOT NULL DEFAULT 'openai' COMMENT 'embedding provider for the index' AFTER llm,
-    ADD COLUMN reranker  VARCHAR(32) NOT NULL DEFAULT 'cohere' COMMENT 'default reranker for the index' AFTER embedding,
+    ADD COLUMN reranker  VARCHAR(32) NOT NULL DEFAULT 'cohere' COMMENT 'default reranker for the index' AFTER llm,
     DROP COLUMN llm_model;
 
 ALTER TABLE `index_query`
