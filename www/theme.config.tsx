19067885952ac6051d909512a13473dbc763a64a
czhen <56986964+shczhen@users.noreply.github.com>
2024-03-14 16:39:29 +0800

    refine www (#85)

--- www/theme.config.tsx
+++ www/theme.config.tsx
@@ -1,6 +1,6 @@
-import Image from 'next/image'
-import { useRouter } from 'next/router'
-import { useConfig } from 'nextra-theme-docs'
+import Image from 'next/image';
+import { useRouter } from 'next/router';
+import { useConfig } from 'nextra-theme-docs';
 
 const themeConfig = {
   docsRepositoryBase: 'https://github.com/pingcap/tidb.ai/tree/main/www',
@@ -19,59 +19,59 @@ const themeConfig = {
     extraContent: (
       <>
         <a
-          style={{ padding: "0.5rem" }}
-          target="_blank"
-          href="https://twitter.com/tidb_developer"
-          aria-label="TiDB Developer Twitter"
-          rel="nofollow noreferrer"
+          style={{ padding: '0.5rem' }}
+          target='_blank'
+          href='https://twitter.com/tidb_developer'
+          aria-label='TiDB Developer Twitter'
+          rel='nofollow noreferrer'
         >
           <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="currentColor"
-            stroke="currentColor"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            className="feather feather-twitter"
+            xmlns='http://www.w3.org/2000/svg'
+            width='24'
+            height='24'
+            viewBox='0 0 24 24'
+            fill='currentColor'
+            stroke='currentColor'
+            strokeWidth='2'
+            strokeLinecap='round'
+            strokeLinejoin='round'
+            className='feather feather-twitter'
           >
-            <path d="M23 3a10.9 10.9 0 0 1-3.14 1.53 4.48 4.48 0 0 0-7.86 3v1A10.66 10.66 0 0 1 3 4s-4 9 5 13a11.64 11.64 0 0 1-7 2c9 5 20 0 20-11.5a4.5 4.5 0 0 0-.08-.83A7.72 7.72 0 0 0 23 3z" />
+            <path d='M23 3a10.9 10.9 0 0 1-3.14 1.53 4.48 4.48 0 0 0-7.86 3v1A10.66 10.66 0 0 1 3 4s-4 9 5 13a11.64 11.64 0 0 1-7 2c9 5 20 0 20-11.5a4.5 4.5 0 0 0-.08-.83A7.72 7.72 0 0 0 23 3z' />
           </svg>
         </a>
       </>
     ),
   },
   head: () => {
-    const { asPath, defaultLocale, locale } = useRouter()
-    const { frontMatter } = useConfig()
+    const { asPath, defaultLocale, locale } = useRouter();
+    const { frontMatter } = useConfig();
     const url =
       'https://tidb.ai' +
-      (defaultLocale === locale ? asPath : `/${locale}${asPath}`)
- 
+      (defaultLocale === locale ? asPath : `/${locale}${asPath}`);
+
     return (
       <>
-        <meta property="og:url" content={url} />
-        <meta property="og:title" content={frontMatter.title || 'TiDB.AI'} />
+        <meta property='og:url' content={url} />
+        <meta property='og:title' content={frontMatter.title || 'TiDB.AI'} />
         <meta
-          property="og:description"
+          property='og:description'
           content={frontMatter.description || 'Docs & Blogs of TiDB.AI'}
         />
         <link
-          rel="shortcut icon"
-          href={`/icon-light.svg`}
-          type="image/svg+xml"
-          media="(prefers-color-scheme: dark)"
+          rel='shortcut icon'
+          href={`/nextra/icon-light.svg`}
+          type='image/svg+xml'
+          media='(prefers-color-scheme: dark)'
         />
         <link
-          rel="shortcut icon"
-          href={`/icon-dark.svg`}
-          type="image/svg+xml"
-          media="(prefers-color-scheme: light)"
+          rel='shortcut icon'
+          href={`/nextra/icon-dark.svg`}
+          type='image/svg+xml'
+          media='(prefers-color-scheme: light)'
         />
       </>
-    )
+    );
   },
   logo: (
     <>
@@ -107,12 +107,27 @@ const themeConfig = {
   ),
   logoLink: 'https://tidb.ai/docs',
   sidebar: {
-    toggleButton: true
+    toggleButton: true,
   },
   footer: {
     text: (
       <span>
-        <Image src='/tidb-logo-w.svg' alt='TiDB' className='logo' width={24} height={24} />
+        <svg
+          xmlns='http://www.w3.org/2000/svg'
+          viewBox='0 0 161.24 186.18'
+          className='logo'
+          style={{
+            width: '24px',
+            height: '24px',
+          }}
+        >
+          <g>
+            <path
+              fill='currentColor'
+              d='M80.62,0L0,46.54v93.09l80.62,46.54,80.62-46.54V46.54L80.62,0ZM80.57,61.98v93.12l-26.77-15.43v-62.24l-26.78,15.46v-30.91l53.54-30.91,26.77,15.45-26.76,15.45ZM134.36,124.12l-26.88,15.52v-62.04l26.88-15.53v62.06Z'
+            />
+          </g>
+        </svg>
         <br />
         {new Date().getFullYear()} ©{' '}
         <a href='https://pingcap.com' target='_blank'>
