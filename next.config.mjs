1f4be2619a3953ad9d5902ba6dc22ef23ba9c8ee
Mini256 <minianter@foxmail.com>
2024-03-28 21:41:20 +0800

    config: allow specify domain access

--- next.config.mjs
+++ next.config.mjs
@@ -27,7 +27,7 @@ let nextConfig = withSvgr(withMDX({
         source: "/api/:path*",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "*" },
+          { key: "Access-Control-Allow-Origin", value: "http://localhost:3001, http://localhost:3000, https://tidb.ai, https://mysql-tips.vercel.app" },
           { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
           { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
         ]
