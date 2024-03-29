8658519c3001c2d3b72cc082135133a9352f0f98
sykp241095 <sykp241095@gmail.com>
2024-03-29 20:53:44 +0800

    docs: add changelog (#93)

--- www/next.config.mjs
+++ www/next.config.mjs
@@ -5,11 +5,29 @@ const nextConfig = {
   reactStrictMode: true,
   basePath: '',
   assetPrefix: process.env.NODE_ENV === 'production' ? '/nextra' : undefined,
+  webpack(config) {
+    const allowedSvgRegex = /components\/icons\/.+\.svg$/
+
+    const fileLoaderRule = config.module.rules.find(rule =>
+      rule.test?.test?.('.svg')
+    )
+    fileLoaderRule.exclude = allowedSvgRegex
+
+    config.module.rules.push({
+      test: allowedSvgRegex,
+      use: ['@svgr/webpack']
+    })
+    return config
+  },
 };
 
 const withNextra = nextra({
   theme: 'nextra-theme-docs',
   themeConfig: './theme.config.tsx',
+  flexsearch: {
+    codeblocks: false
+  },
+  defaultShowCopyCode: true,
 });
 
 export default withNextra(nextConfig);
