7701fa4cfc2bee50439af66a804c08146e6a6410
Mini256 <minianter@foxmail.com>
2024-03-29 11:59:38 +0800

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -27,7 +27,7 @@ let nextConfig = withSvgr(withMDX({
         source: "/api/:path*",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "http://localhost:3000" },
+          { key: "Access-Control-Allow-Origin", value: "http://127.0.0.1:3000" },
           { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
           { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, X-Recaptcha-Token, X-Recaptcha-Action, X-CreateRag-Session, X-Experimental-Stream-Data" },
           { key: "Access-Control-Expose-Headers", value: "X-Experimental-Stream-Data" }
