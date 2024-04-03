2d0579ab7ea62927677707880697bf7e005b2295
sykp241095 <sykp241095@gmail.com>
2024-04-03 15:07:33 +0800

    docs: refactor how it works (#95)

--- www/theme.config.tsx
+++ www/theme.config.tsx
@@ -6,7 +6,7 @@ const themeConfig = {
   docsRepositoryBase: 'https://github.com/pingcap/tidb.ai/tree/main/www',
   useNextSeoProps() {
     return {
-      titleTemplate: '%s - RAG by TiDB ',
+      titleTemplate: '%s - RAG app by TiDB ',
     };
   },
   project: {
@@ -38,6 +38,29 @@ const themeConfig = {
             className='feather feather-twitter'
           >
             <path d='M23 3a10.9 10.9 0 0 1-3.14 1.53 4.48 4.48 0 0 0-7.86 3v1A10.66 10.66 0 0 1 3 4s-4 9 5 13a11.64 11.64 0 0 1-7 2c9 5 20 0 20-11.5a4.5 4.5 0 0 0-.08-.83A7.72 7.72 0 0 0 23 3z' />
+          </svg>
+        </a>
+        <a
+          style={{ padding: '0.5rem' }}
+          target='_blank'
+          href='https://tidb.cloud'
+          aria-label='TiDB Cloud'
+          rel='nofollow noreferrer'
+        >
+          <svg
+            xmlns='http://www.w3.org/2000/svg'
+            width='24'
+            height='24'
+            viewBox='0 0 161.24 186.18'
+            // viewBox='0 0 24 24'
+            fill='currentColor'
+            stroke='currentColor'
+            strokeWidth='2'
+            strokeLinecap='round'
+            strokeLinejoin='round'
+            className='feather feather-tidb'
+          >
+            <path fill="currentColor" d="M80.62,0L0,46.54v93.09l80.62,46.54,80.62-46.54V46.54L80.62,0ZM80.57,61.98v93.12l-26.77-15.43v-62.24l-26.78,15.46v-30.91l53.54-30.91,26.77,15.45-26.76,15.45ZM134.36,124.12l-26.88,15.52v-62.04l26.88-15.53v62.06Z"/>
           </svg>
         </a>
       </>
