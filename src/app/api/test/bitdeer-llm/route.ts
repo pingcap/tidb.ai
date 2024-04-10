dcf1d622a1dd1ffce4222e718da751227744ae81
Mini256 <minianter@foxmail.com>
2024-04-10 17:50:58 +0800

    feat: add bitdeer llm

---
+++ src/app/api/test/bitdeer-llm/route.ts
@@ -0,0 +1,79 @@
+import {Bitdeer} from "@/lib/llamaindex/llm/bitdeer";
+import {LLMRerank} from "@/lib/llamaindex/postprocessors/rerankers/LLMReranker";
+import {TextNode} from "llamaindex";
+import {NextRequest, NextResponse} from 'next/server';
+
+const testNodes: any[] = [
+  {
+    node: new TextNode({
+      id_: '101',
+      text: 'Redis is an in-memory data structure store.',
+    })
+  },
+  {
+    node: new TextNode({
+      id_: '102',
+      text: 'TiDB is a distributed, MySQL-compatible database.',
+    })
+  },
+  {
+    node: new TextNode({
+      id_: '103',
+      text: 'Elasticsearch is a distributed, RESTful search and analytics engine.',
+    })
+  },
+  {
+    node: new TextNode({
+      id_: '104',
+      text: 'Neo4j is a graph database management system.',
+    })
+  },
+  {
+    node: new TextNode({
+      id_: '105',
+      text: 'MongoDB is a cross-platform document-oriented database program.',
+    })
+  },
+  {
+    node: new TextNode({
+      id_: '106',
+      text: 'Cassandra is a distributed NoSQL database management system.',
+    })
+  },
+  {
+    node: new TextNode({
+      id_: '107',
+      text: 'PostgreSQL is a powerful, open-source object-relational database system.',
+    })
+  },
+  {
+    node: new TextNode({
+      id_: '108',
+      text: 'SQLite is a C-language library that implements a small, fast, self-contained, high-reliability, full-featured, SQL database engine.',
+    })
+  },
+  {
+    node: new TextNode({
+      id_: '109',
+      text: 'MariaDB is a community-developed, commercially supported fork of the MySQL relational database management system.',
+    })
+  }
+];
+
+export async function GET (req: NextRequest) {
+  const url = new URL(req.url);
+  const query = url.searchParams.get('query') || 'I want a database to replace MySQL.';
+  const llm = new Bitdeer({
+    model: 'mistral',
+    apiSecretAccessKey: process.env.BITDEER_API_SECRET_ACCESS_KEY!
+  })
+  const reranker = new LLMRerank({
+    llm
+  });
+
+  const nodesWithScores = await reranker.postprocessNodes(testNodes, query);
+
+  return NextResponse.json(nodesWithScores);
+}
+
+export const dynamic = 'force-dynamic';
---
+++ src/lib/llamaindex/llm/bitdeer.ts
@@ -0,0 +1,191 @@
+import {ok} from "node:assert";
+import {
+  BaseEmbedding, ChatResponse,
+  ChatResponseChunk,
+  CompletionResponse,
+  LLM, LLMChatParamsNonStreaming,
+  LLMChatParamsStreaming, LLMCompletionParamsNonStreaming, LLMCompletionParamsStreaming,
+  LLMMetadata
+} from "llamaindex";
+
+const messageAccessor = (data: any): ChatResponseChunk => {
+  return {
+    delta: data.message.content,
+  };
+};
+
+const completionAccessor = (data: any): CompletionResponse => {
+  return { text: data.response };
+};
+
+export type BitdeerModel = "llama2" | "mistral";
+
+export interface BitdeerLlama2Options extends Record<string, unknown> {
+  microstat: number;
+  mirostat_eta: number;
+  mirostat_tau: number;
+  num_ctx: number;
+  repeat_last_n: number;
+  repeat_penalty: number;
+  temperature: number;
+  num_predict: number;
+  seed: number;
+  tfs_z: number;
+  top_k: number;
+  top_p: number;
+}
+
+export type BitdeerCompletionOptions = BitdeerLlama2Options;
+
+/**
+ * Bitdeer is a cloud computing platform that provides computing power for cryptocurrency mining.
+ *
+ * In the AI age, Bitdeer also provides managed AI models (For example, llama2, mistral, etc.) services.
+ *
+ * Website: https://www.bitdeer.com/
+ */
+export class Bitdeer extends BaseEmbedding implements LLM<BitdeerCompletionOptions> {
+  readonly hasStreaming = true;
+
+  model: string;
+  baseURL: string = "https://www.bitdeer.ai/public/v1";
+  temperature: number = 0.7;
+  topP: number = 0.9;
+  contextWindow: number = 4096;
+  requestTimeout: number = 60 * 1000; // Default is 60 seconds
+  additionalChatOptions?: Record<string, unknown>;
+  additionalCompletionOptions?: Record<string, unknown>;
+
+  private apiSecretAccessKey: string;
+
+  protected modelMetadata: Partial<LLMMetadata>;
+
+  constructor(
+    init: Partial<Bitdeer> & {
+      // model is required
+      model: BitdeerModel;
+      modelMetadata?: Partial<LLMMetadata>;
+      apiSecretAccessKey: string;
+    },
+  ) {
+    super();
+
+    if (!init.apiSecretAccessKey) {
+      throw new Error("API secret access key is required.");
+    }
+
+    this.model = init.model ?? 'llama2';
+    this.modelMetadata = init.modelMetadata ?? {};
+    this.apiSecretAccessKey = init.apiSecretAccessKey;
+    Object.assign(this, init);
+  }
+
+  get metadata(): LLMMetadata {
+    return {
+      model: this.model,
+      temperature: this.temperature,
+      topP: this.topP,
+      maxTokens: undefined,
+      contextWindow: this.contextWindow,
+      tokenizer: undefined,
+      ...this.modelMetadata,
+    };
+  }
+
+  chat(
+    params: LLMChatParamsStreaming,
+  ): Promise<AsyncIterable<ChatResponseChunk>>;
+  chat(params: LLMChatParamsNonStreaming): Promise<ChatResponse>;
+  async chat(
+    params: LLMChatParamsNonStreaming | LLMChatParamsStreaming,
+  ): Promise<ChatResponse | AsyncIterable<ChatResponseChunk>> {
+    // Notice: Bitdeer does not support chat API for now.
+    throw new Error("Method not implemented.");
+  }
+
+  private async *streamChat<T>(
+    stream: ReadableStream<Uint8Array>,
+    accessor: (data: any) => T,
+  ): AsyncIterable<T> {
+    const reader = stream.getReader();
+    while (true) {
+      const { done, value } = await reader.read();
+      if (done) {
+        return;
+      }
+      const lines = Buffer.from(value)
+        .toString("utf-8")
+        .split("\n")
+        .map((line) => line.trim());
+      for (const line of lines) {
+        if (line === "") {
+          continue;
+        }
+        const json = JSON.parse(line);
+        if (json.error) {
+          throw new Error(json.error);
+        }
+        yield accessor(json);
+      }
+    }
+  }
+
+  complete(
+    params: LLMCompletionParamsStreaming,
+  ): Promise<AsyncIterable<CompletionResponse>>;
+  complete(
+    params: LLMCompletionParamsNonStreaming,
+  ): Promise<CompletionResponse>;
+  async complete(
+    params: LLMCompletionParamsStreaming | LLMCompletionParamsNonStreaming,
+  ): Promise<CompletionResponse | AsyncIterable<CompletionResponse>> {
+    const { prompt, stream } = params;
+    const payload = {
+      model: this.model,
+      prompt: prompt,
+      stream: !!stream,
+      options: {
+        temperature: this.temperature,
+        num_ctx: this.contextWindow,
+        top_p: this.topP,
+        ...this.additionalCompletionOptions,
+      },
+    };
+    const response = await fetch(`${this.baseURL}/models/${this.model}/generate`, {
+      body: JSON.stringify(payload),
+      method: "POST",
+      // signal: AbortSignal.timeout(this.requestTimeout),
+      headers: {
+        "Content-Type": "application/json",
+        "X-Api-Key": this.apiSecretAccessKey,
+      },
+    });
+
+    if (!stream) {
+      const raw = await response.json();
+      console.log(raw.data.response)
+      return {
+        text: raw.data.response,
+        raw,
+      };
+    } else {
+      const stream = response.body;
+      ok(stream, "stream is null");
+      ok(stream instanceof ReadableStream, "stream is not readable");
+      return this.streamChat(stream, completionAccessor);
+    }
+  }
+
+  private async getEmbedding(prompt: string): Promise<number[]> {
+    // Notice: Bitdeer does not support embedding API for now.
+    throw new Error("Method not implemented.");
+  }
+
+  async getTextEmbedding(text: string): Promise<number[]> {
+    return this.getEmbedding(text);
+  }
+
+  async getQueryEmbedding(query: string): Promise<number[]> {
+    return this.getEmbedding(query);
+  }
+}
