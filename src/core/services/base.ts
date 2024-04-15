ef33d7ea8855588d77d49980de3c50812228f133
Jagger <634750802@qq.com>
2024-04-15 11:32:30 +0800

    move import processor to DocumentImportService

--- src/core/services/base.ts
+++ src/core/services/base.ts
@@ -1,17 +1,27 @@
 import { Flow } from '@/core';
 import type { Index } from '@/core/repositories/index_';
 
-export interface AppIndexBaseServiceOptions {
+export interface AppFlowBaseServiceOptions {
   flow: Flow;
+}
+
+export interface AppIndexBaseServiceOptions extends AppFlowBaseServiceOptions {
   index: Index;
 }
 
-export abstract class AppIndexBaseService {
+export abstract class AppFlowBaseService {
   protected readonly flow: Flow;
+
+  constructor (options: AppFlowBaseServiceOptions) {
+    this.flow = options.flow;
+  }
+}
+
+export abstract class AppIndexBaseService extends AppFlowBaseService {
   protected readonly index: Index;
 
   constructor (options: AppIndexBaseServiceOptions) {
-    this.flow = options.flow;
+    super(options);
     this.index = options.index;
   }
 }
