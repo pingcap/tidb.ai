a6fc9b51b4fe855eb3abf0294c69dd5f1956fd48
sykp241095 <sykp241095@gmail.com>
2024-04-16 11:05:15 +0800

    chore: add widget to tidb.ai/docs

--- www/pages/_document.tsx
+++ www/pages/_document.tsx
@@ -5,6 +5,19 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <link rel="shortcut icon" href="/docs/icon-black.svg" />
+        <script
+          async
+          src='https://tidb.ai/rag-widget.js'
+          data-id='tidb-ai-widget'
+          data-name='tidb-ai-widget'
+          data-btn-label='Ask AI'
+          data-btn-img-src='https://tidb.ai/tidb-ai.svg'
+          data-logo-src='https://tidb.ai/tidb-ai.svg'
+          data-preferred-mode='system'
+          data-widget-title='Conversation Search Box'
+          data-widget-input-placeholder='Ask a question...'
+          data-example-questions='["What is TiDB?","Does TiDB support FOREIGN KEY?","Does TiDB support serverless?"]'
+        />
       </Head>
       <body>
         <Main />
