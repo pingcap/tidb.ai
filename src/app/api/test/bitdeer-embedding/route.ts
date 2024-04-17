377363598c7ca8a7a870ea08bd00220e30991715
Mini256 <minianter@foxmail.com>
2024-04-17 12:12:02 +0800

    fix: support config bitdeer embedding api key via env (#112)

--- src/app/api/test/bitdeer-embedding/route.ts
+++ src/app/api/test/bitdeer-embedding/route.ts
@@ -1,5 +1,4 @@
 import {getEmbedding} from "@/lib/llamaindex/converters/embedding";
-import {BitdeerEmbedding, BitdeerEmbeddingModelType} from "@/lib/llamaindex/embeddings/BitdeerEmbedding";
 import {baseRegistry} from "@/rag-spec/base";
 import {getFlow} from "@/rag-spec/createFlow";
 import {NextRequest, NextResponse} from 'next/server';
@@ -9,10 +8,7 @@ const flow = await getFlow(baseRegistry);
 export async function GET (req: NextRequest) {
   const url = new URL(req.url);
   const input = url.searchParams.get('input') || 'I want a database to replace MySQL.';
-  const bitdeerEmbedding = getEmbedding(flow, 'bitdeer', {
-    model: BitdeerEmbeddingModelType.MISTRAL_EMBED_LARGE,
-    apiSecretAccessKey: process.env.BITDEER_API_SECRET_ACCESS_KEY!
-  });
+  const bitdeerEmbedding = getEmbedding(flow, 'bitdeer', {});
 
   const embedding = await bitdeerEmbedding.getQueryEmbedding(input);
 
