b83b6c6309b64a9df551bf095bb5d03a28ba1171
Mini256 <minianter@foxmail.com>
2024-05-28 17:07:51 +0800

    feat: add liquid buildToolText filter

--- src/lib/llamaindex/prompts/PromptParser.ts
+++ src/lib/llamaindex/prompts/PromptParser.ts
@@ -1,8 +1,12 @@
 import {Liquid} from "liquidjs";
-import {SimplePrompt} from "llamaindex";
+import {buildToolsText, SimplePrompt} from "llamaindex";
 
 export class PromptParser {
   private liquid = new Liquid();
+
+  constructor() {
+    this.liquid.registerFilter('buildToolsText', buildToolsText);
+  }
 
   getPrompt<Tmpl extends SimplePrompt> (template: string | undefined, fallback: Tmpl, partialContext?: Record<string, any>): (ctx: Parameters<Tmpl>[0]) => string {
     if (!template) return fallback;
