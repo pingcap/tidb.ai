981310ae8e04ae1d05bc028b98c6200eaf98b723
Jagger <634750802@qq.com>
2024-06-11 16:34:27 +0800

    fix: create graph entity issue (#158)

---
+++ src/components/graph/components/SearchEntityById.tsx
@@ -0,0 +1,97 @@
+import { EntitiesTable, entityColumns } from '@/components/graph/components/EntitiesTable';
+import { type UseEntitiesRequired, useGraphEntitiesByIdsTable } from '@/components/graph/selectEntities';
+import { Button } from '@/components/ui/button';
+import { Dialog, DialogContent, DialogHeader, DialogPortal, DialogTitle, DialogTrigger } from '@/components/ui/dialog';
+import { Form, FormControl, FormField } from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
+import { cn } from '@/lib/utils';
+import { zodResolver } from '@hookform/resolvers/zod';
+import isHotkey from 'is-hotkey';
+import { PlusIcon } from 'lucide-react';
+import { useForm } from 'react-hook-form';
+import z from 'zod';
+
+export interface SearchEntityProps extends UseEntitiesRequired {
+}
+
+export function SearchEntityById ({ ...props }: SearchEntityProps) {
+  const { table, ids, setIds } = useGraphEntitiesByIdsTable({
+    columns: entityColumns,
+    ...props,
+  });
+
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button variant='secondary'>
+          <PlusIcon className="mr-2 w-3.5 h-3.5" />
+          Entities by IDs
+        </Button>
+      </DialogTrigger>
+      <DialogPortal>
+        <DialogContent className="overflow-x-hidden max-w-screen-md space-y-2">
+          <DialogHeader>
+            <DialogTitle>
+              Search entities by IDs
+            </DialogTitle>
+          </DialogHeader>
+          <TableFilterForm filter={ids} onFilterChange={setIds} />
+          <EntitiesTable table={table} isLoading={false} />
+        </DialogContent>
+      </DialogPortal>
+    </Dialog>
+  );
+}
+
+function TableFilterForm ({ className, filter, onFilterChange, disabled }: { className?: string, filter: number[], onFilterChange: (filter: number[]) => void, disabled?: boolean }) {
+  const form = useForm<{ ids: string }>({
+    values: { ids: filter.map(i => String(i)).join(', ') },
+    resolver: zodResolver(tableFilterSchema),
+    disabled,
+    defaultValues: {
+      ids: '',
+    },
+  });
+
+  const populate = () => {
+    const ids = form.getValues().ids.split(',')
+      .map(s => s.trim())
+      .filter(Boolean)
+      .map(s => parseInt(s))
+      .filter(n => isFinite(n));
+    onFilterChange(ids);
+  };
+
+  return (
+    <Form {...form}>
+      <form className={cn('flex gap-2 items-center', className)} onSubmit={event => {
+        event.stopPropagation();
+        event.preventDefault();
+        return false;
+      }}>
+        <FormField
+          name="ids"
+          render={({ field }) => (
+            <FormControl>
+              <Input
+                {...field}
+                placeholder="Input entities ids separated by ','"
+                onKeyDown={ev => {
+                  if (isHotkey('Enter', ev)) {
+                    ev.stopPropagation();
+                    ev.preventDefault();
+                    populate();
+                  }
+                }}
+              />
+            </FormControl>
+          )}
+        />
+      </form>
+    </Form>
+  );
+}
+
+const tableFilterSchema = z.object({
+  ids: z.string(),
+});
---
+++ src/components/graph/selectEntities.ts
@@ -0,0 +1,175 @@
+import { type Entity, handleServerEntity, type ServerEntity } from '@/components/graph/api';
+import type { RemoteEntity } from '@/components/graph/components/EntitiesTable';
+import { fetcher, handleErrors } from '@/lib/fetch';
+import { type ColumnDef, getCoreRowModel, useReactTable } from '@tanstack/react-table';
+import { type RowSelectionState } from '@tanstack/table-core';
+import { type Dispatch, type MutableRefObject, type SetStateAction, useEffect, useMemo, useRef, useState } from 'react';
+import useSWR from 'swr';
+
+export type SearchEntityFilter = {
+  query: string;
+  top_k?: number
+}
+
+function shouldFetch (filter: SearchEntityFilter) {
+  return !!filter.query.trim();
+}
+
+export type UseEntitiesRequired = Pick<ReturnType<typeof useEntities>, 'selectedEntities' | 'rowSelection' | 'setRowSelection' | 'entityMap'>
+
+export function useEntities () {
+  const [rowSelection, setRowSelection] = useState<RowSelectionState>({});
+  const entityMap = useRef<Map<string, Entity>>(undefined as never);
+  if (!entityMap.current) {
+    entityMap.current = new Map<string, Entity>();
+  }
+
+  const selectedEntities = useMemo(() => {
+    return Object.keys(rowSelection).map(id => entityMap.current.get(id)).filter(Boolean) as Entity[];
+  }, [rowSelection]);
+
+  const clearSelection = (id?: number) => {
+    if (id == null) {
+      setRowSelection({});
+    } else {
+      setRowSelection(selection => {
+        const idStr = String(id);
+        if (idStr in selection) {
+          selection = { ...selection };
+          delete selection[idStr];
+        }
+
+        return selection;
+      });
+    }
+  };
+
+  return {
+    selectedEntities,
+    rowSelection,
+    setRowSelection,
+    clearSelection,
+    entityMap,
+  };
+}
+
+export function useGraphEntitiesTable ({ rowSelection, setRowSelection, entityMap, columns }: { rowSelection: RowSelectionState, setRowSelection: Dispatch<SetStateAction<RowSelectionState>>, entityMap: MutableRefObject<Map<string, RemoteEntity>>, columns: ColumnDef<RemoteEntity, any>[] }) {
+  const [filter, setFilter] = useState<SearchEntityFilter>(() => ({ query: '', top_k: undefined }));
+
+  const { data, isLoading, error } = useSWR(shouldFetch(filter) && ['get', '/api/v1/indexes/graph/entities/search', filter], fetcher<ServerEntity[]>, {
+    revalidateOnFocus: false,
+  });
+
+  const entities = useMemo(() => {
+    if (data) {
+      return data.map(serverEntity => {
+        const entity = handleServerEntity(serverEntity);
+        entityMap.current.set(String(entity.id), entity);
+        return entity;
+      });
+    } else {
+      return [];
+    }
+  }, [data]);
+
+  const table = useReactTable<RemoteEntity>({
+    data: entities,
+    getRowId: row => String(row.id),
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+    enableRowSelection: true,
+    enableMultiRowSelection: true,
+    state: {
+      rowSelection,
+    },
+    onRowSelectionChange: setRowSelection,
+  });
+
+  return {
+    table,
+    filter,
+    setFilter,
+    isLoading,
+    error,
+  };
+}
+
+export function useGraphEntitiesByIdsTable ({ rowSelection, setRowSelection, entityMap, columns }: { rowSelection: RowSelectionState, setRowSelection: Dispatch<SetStateAction<RowSelectionState>>, entityMap: MutableRefObject<Map<string, RemoteEntity>>, columns: ColumnDef<RemoteEntity, any>[] }) {
+  const [ids, setIds] = useState<number[]>([]);
+  const [data, setData] = useState<RemoteEntity[]>([]);
+  const promisesRef = useRef<Record<string, Promise<void>>>({});
+  const [failed, setFailed] = useState<Record<string, unknown>>({});
+  const idsRef = useRef(ids);
+
+  useEffect(() => {
+    idsRef.current = ids;
+  });
+
+  useEffect(() => {
+    for (const id of ids) {
+      let entity = entityMap.current.get(String(id));
+      if (!entity) {
+        if (!promisesRef.current[String(id)]) {
+          promisesRef.current[String(id)] = fetch(`/api/v1/indexes/graph/entities/${id}`)
+            .then(handleErrors)
+            .then(res => res.json())
+            .then(handleServerEntity)
+            .then(
+              entity => {
+                entityMap.current.set(String(entity.id), entity);
+                updateData();
+              },
+              error => {
+                entityMap.current.set(String(id), {
+                  id: id,
+                  isLoading: false,
+                  error,
+                });
+                setFailed(failed => ({
+                  ...failed,
+                  [String(id)]: error,
+                }));
+                updateData();
+              },
+            );
+        }
+      }
+    }
+    updateData();
+  }, [ids]);
+
+  const updateData = () => {
+    setData(idsRef.current.map(id => {
+      const entity = entityMap.current.get(String(id));
+      if (!entity) {
+        return { id, isLoading: true };
+      } else {
+        return entity;
+      }
+    }));
+  };
+
+  const table = useReactTable<RemoteEntity>({
+    data,
+    getRowId: row => String(row.id),
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+    enableRowSelection: true,
+    enableMultiRowSelection: true,
+    state: {
+      rowSelection,
+    },
+    onRowSelectionChange: setRowSelection,
+  });
+
+  return {
+    table,
+    ids,
+    setIds,
+    failed,
+  };
+}
+
+
+
+
