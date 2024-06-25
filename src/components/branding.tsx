04a8e10e974dde40c79cd63d0c711febbbc8846b
Jagger <634750802@qq.com>
2024-06-25 14:31:21 +0800

    ui: auto logo width

--- src/components/branding.tsx
+++ src/components/branding.tsx
@@ -3,8 +3,8 @@ import {IWebsiteSettingResult} from "@/core/schema/settings/website";
 export function Branding ({ setting }: { setting: IWebsiteSettingResult }) {
   return (
     <span className="flex items-center gap-4">
-      <img className="dark:hidden" src={setting.logo_in_light_mode} alt="logo" width={32} height={32} />
-      <img className="hidden dark:block" src={setting.logo_in_dark_mode} alt="logo" width={32} height={32} />
+      <img className="dark:hidden" src={setting.logo_in_light_mode} alt="logo" height={32} />
+      <img className="hidden dark:block" src={setting.logo_in_dark_mode} alt="logo" height={32} />
       <span className="text-2xl font-light flex-shrink-0 tracking-widest">
         {setting.title}
       </span>
