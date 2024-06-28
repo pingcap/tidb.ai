9f81e162a66a4637671941cc1fb6666189c2fbac
Jagger <634750802@qq.com>
2024-06-28 15:34:40 +0800

    ui: change logo

--- src/components/branding.tsx
+++ src/components/branding.tsx
@@ -5,9 +5,9 @@ export function Branding ({ setting }: { setting: IWebsiteSettingResult }) {
     <span className="flex items-center gap-4">
       <img className="h-8 dark:hidden" src={setting.logo_in_light_mode} alt="logo" />
       <img className="h-8 hidden dark:block" src={setting.logo_in_dark_mode} alt="logo" />
-      <span className="text-2xl font-light flex-shrink-0 tracking-widest">
-        {setting.title}
-      </span>
+      {/*<span className="text-2xl font-light flex-shrink-0 tracking-widest">*/}
+      {/*  {setting.title}*/}
+      {/*</span>*/}
     </span>
   );
 }
