9c9175706aa1f348b99f53e1dc4023d74c1cf304
Jagger <634750802@qq.com>
2024-07-30 19:03:23 +0800

    ui(frontend): show git revision status

--- frontend/app/src/components/VersionStatus.tsx
+++ frontend/app/src/components/VersionStatus.tsx
@@ -1,7 +1,5 @@
-// GET https://api.github.com/repos/pingcap/tidb.ai/compare/399279a0a6838dfe109ef627138a3612b915f1fd...d5d536
-// X-GitHub-Api-Version: 2022-11-28
-
-import { handleErrors } from '@/lib/request';
+import { handleErrors, ServerError } from '@/lib/request';
+import { cn } from '@/lib/utils';
 
 type Status = {
   html_url: string
@@ -28,15 +26,25 @@ export async function VersionStatus ({ gitCommitHash }: VersionStatusProps) {
     const data: Status = await response.json();
     switch (data.status) {
       case 'identical':
-        return <span>Updated</span>;
+        return <span><StatusDot className="bg-green-500" />Updated</span>;
       case 'ahead':
-        return <span>{data.ahead_by} commits ahead</span>;
+        return <span><StatusDot className="text-yellow-500" />{data.ahead_by} commits ahead</span>;
       case 'behind':
-        return <span>{data.behind_by} commits behind</span>;
+        return <span><StatusDot className="text-yellow-500" />{data.behind_by} commits behind</span>;
       case 'diverged':
-        return <span>{data.ahead_by} commits ahead, {data.behind_by} commits behind.</span>;
+        return <span><StatusDot className="text-yellow-500" />{data.ahead_by} commits ahead, {data.behind_by} commits behind.</span>;
     }
-  } catch {
-    return <span className="text-xs text-muted-foreground">Failed to get revision info</span>;
+  } catch (e) {
+    if (e instanceof ServerError) {
+      if (e.response.status === 404) {
+        return <span className="text-xs text-muted-foreground"><StatusDot className="text-zink-500" />Revision not found</span>;
+      }
+    }
+    console.error(e);
+    return <span className="text-xs text-muted-foreground"><StatusDot className="text-red-500" />Failed to get revision info</span>;
   }
+}
+
+function StatusDot ({ className }: { className: string }) {
+  return <span className={cn('inline-block w-1.5 h-1.5 rounded-full mr-1 bg-current', className)} />;
 }
