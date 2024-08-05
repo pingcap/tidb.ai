b2fcd7922ceacec96f9354cee399726407607c6b
Cheese <cheese.javaee@gmail.com>
2024-08-05 11:33:02 +0800

    feat: add retrieve api (#207)
    Added an API to retrieve `Document`s from related `Chuck`s.

---
+++ backend/app/api/admin_routes/retrieve.py
@@ -0,0 +1,20 @@
+from typing import Optional, List
+
+from fastapi import APIRouter
+from app.models import Document
+from app.api.deps import SessionDep, CurrentSuperuserDep
+from app.rag.retrieve import RetrieveService
+
+router = APIRouter()
+
+
+@router.get("/admin/retrieve/documents")
+async def retrieve_documents(
+    session: SessionDep,
+    user: CurrentSuperuserDep,
+    question: str,
+    chat_engine: str = "default",
+    top_k: Optional[int] = 5,
+) -> List[Document]:
+    retrieve_service = RetrieveService(session, chat_engine)
+    return retrieve_service.retrieve(question, top_k=top_k)
