7f60a17639f982d417d850ad610be831be2b7830
Jagger <634750802@qq.com>
2024-08-23 10:50:38 +0800

    feat(widget): support config base url

--- frontend/app/src/components/settings/WidgetSnippet.tsx
+++ frontend/app/src/components/settings/WidgetSnippet.tsx
@@ -11,7 +11,7 @@ export function WidgetSnippet () {
 
   const [url, setUrl] = useState(base + '/widget.js');
 
-  const script = `<script src="${url}" async></script>`
+  const script = `<script src="${url}" data-api-base="${new URL(url).origin}" async></script>`;
 
   useEffect(() => {
     if (!process.env.SITE_URL) {
@@ -21,13 +21,13 @@ export function WidgetSnippet () {
 
   return (
     <div className="rounded-lg p-2 border mb-4 relative bg-foreground/5">
-      <div className='text-xs font-mono opacity-30 select-none'>&lt;<span className='hljs-name'>html</span>&gt;</div>
-      <div className='text-xs font-mono opacity-30 select-none'>&lt;<span className="hljs-name">body</span>&gt;</div>
-      <div className='text-xs font-mono opacity-30 select-none whitespace-pre'>  ...</div>
+      <div className="text-xs font-mono opacity-30 select-none">&lt;<span className="hljs-name">html</span>&gt;</div>
+      <div className="text-xs font-mono opacity-30 select-none">&lt;<span className="hljs-name">body</span>&gt;</div>
+      <div className="text-xs font-mono opacity-30 select-none whitespace-pre"> ...</div>
       <HtmlViewer value={`  ${script}`} />
       <div className="text-xs font-mono opacity-30 select-none">&lt;/<span className="hljs-name">body</span>&gt;</div>
-      <div className='text-xs font-mono opacity-30 select-none'>&lt;/<span className='hljs-name'>html</span>&gt;</div>
-      <Button variant="secondary" size='sm' className='absolute top-0.5 right-0.5 select-none' onClick={() => {
+      <div className="text-xs font-mono opacity-30 select-none">&lt;/<span className="hljs-name">html</span>&gt;</div>
+      <Button variant="secondary" size="sm" className="absolute top-0.5 right-0.5 select-none" onClick={() => {
         navigator.clipboard.writeText(script);
         setCopied(true);
       }}>
--- frontend/app/src/lib/request.ts
+++ frontend/app/src/lib/request.ts
@@ -2,7 +2,24 @@ import { z, ZodType } from 'zod';
 
 let BASE_URL: string;
 
-if (typeof process.env.BASE_URL !== 'undefined') {
+if (process.env.NEXT_PUBLIC_IS_WIDGET) {
+  const script = document.currentScript as HTMLScriptElement | null;
+  if (!script) {
+    throw new Error(`Widget not supported in this browser (evaluating document.currentScript)`);
+  }
+  // data-api-base
+  if (script.dataset.apiBase) {
+    BASE_URL = script.dataset.apiBase;
+    console.debug('[tidbai.widget]', 'widget base url resolved by "data-api-base" attribute', BASE_URL);
+  } else if (/^https?:\/\//.test(script.src)) {
+    const scriptUrl = new URL(script.src);
+    BASE_URL = scriptUrl.origin;
+    console.debug('[tidbai.widget]', 'widget base url resolved by script origin', BASE_URL);
+  } else {
+    console.warn(`Add attribute "data-api-base"="YOUR_HOST" to your widget script tag.`);
+    throw new Error(`Cannot initialize widget.`);
+  }
+} else if (typeof process.env.BASE_URL !== 'undefined') {
   BASE_URL = process.env.BASE_URL;
 } else {
   BASE_URL = '';
