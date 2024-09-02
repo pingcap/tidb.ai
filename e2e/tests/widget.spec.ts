ae9e6cda35bae393a87fb9e263bcb4fb08a16938
Jagger <634750802@qq.com>
2024-09-02 11:03:52 +0800

    remove test.only

--- e2e/tests/widget.spec.ts
+++ e2e/tests/widget.spec.ts
@@ -6,7 +6,7 @@ test('JS Widget', async ({ page }) => {
   expect(await page.evaluate('window.tidbai')).toMatchObject({ open: false });
 });
 
-test.only('JS Widget from other page', async ({ page }) => {
+test('JS Widget from other page', async ({ page }) => {
   await page.goto('http://localhost:4001/widget.html');
   await page.getByRole('button', { name: 'Ask AI' }).waitFor({ state: 'visible' });
   expect(await page.evaluate('window.tidbai')).toMatchObject({ open: false });
