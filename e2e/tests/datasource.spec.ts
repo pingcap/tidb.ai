185f2a9c2b127bd9c57a353d55b91ad8523fefc0
Jagger <634750802@qq.com>
2024-09-13 10:11:29 +0800

    e2e: add sitemap test (#280)

--- e2e/tests/datasource.spec.ts
+++ e2e/tests/datasource.spec.ts
@@ -50,8 +50,50 @@ test.describe('Datasource', () => {
     });
   });
 
-  test('Web Sitemap', () => {
-    test.fixme(true, 'Find a sample site with sitemap.xml');
+  test('Web Sitemap', async ({ page }) => {
+    test.slow();
+
+    await test.step('Login and visit page', async () => {
+      await loginViaApi(page);
+
+      await page.goto('/datasources');
+      await expect(page.getByRole('heading', { name: 'Datasources' })).toBeVisible();
+    });
+
+    await test.step('Add Sitemap Datasource', async () => {
+      await page.getByRole('button', { name: 'Create' }).click();
+      await page.getByRole('tab', { name: 'Web Sitemap' }).click();
+      await page.waitForURL('/datasources/create/web-sitemap');
+
+      await page.getByLabel('Name').fill('example site from sitemap');
+      await page.getByLabel('Description').fill('This is example sitemap');
+
+      await page.locator('input[name="url"]').fill('http://static-web-server/example-sitemap.xml');
+
+      await page.getByRole('button', { name: 'Create Datasource' }).click();
+
+      await page.waitForURL(/\/datasources\/\d+/);
+
+      const id = /\/datasources\/(\d+)/.exec(page.url())[1];
+      while (true) {
+        const response = await page.request.get(`/api/v1/admin/datasources/${id}/overview`);
+        if (response.ok()) {
+          const json = await response.json();
+          if (json.vector_index.completed === 2) {
+            break;
+          }
+        } else {
+          console.warn(`${response.status()} ${response.statusText()}`, await response.text());
+        }
+        await page.waitForTimeout(500);
+      }
+    });
+
+    await test.step('Check Documents Page', async () => {
+      await page.goto('/documents');
+      await expect(page.getByRole('link', { name: 'http://static-web-server/example-doc-1.html' })).toBeVisible();
+      await expect(page.getByRole('link', { name: 'http://static-web-server/example-doc-2.html' })).toBeVisible();
+    });
   });
 
   test('Files', () => {
