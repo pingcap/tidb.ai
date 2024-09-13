cb4ff98bfc64d4e2d7b4e7fbec01c33e43f3b7fc
Jagger <634750802@qq.com>
2024-09-13 10:28:54 +0800

    e2e: add widget chat tests (#279)

--- e2e/tests/chat.spec.ts
+++ e2e/tests/chat.spec.ts
@@ -1,6 +1,5 @@
-import { expect, type Page, type Request, test } from '@playwright/test';
-
-const QUESTION = 'What is the content of sample.pdf?';
+import { expect, test } from '@playwright/test';
+import { getChatRequestPromise, QUESTION, testNewChat } from '../utils/chat';
 
 test.describe('Chat', () => {
   test('From Home Page', async ({ page, baseURL }) => {
@@ -12,7 +11,7 @@ test.describe('Chat', () => {
       await page.getByPlaceholder('Input your question here...').fill(QUESTION);
 
       // https://playwright.dev/docs/events#waiting-for-event
-      const chatRequestPromise = page.waitForRequest(request => request.url() === `${baseURL}/api/v1/chats` && request.method() === 'POST');
+      const chatRequestPromise = getChatRequestPromise(page, baseURL);
       const trigger = page.locator('button', { has: page.locator('svg.lucide-arrow-right') });
       await trigger.click();
 
@@ -20,7 +19,7 @@ test.describe('Chat', () => {
       return await chatRequestPromise;
     });
 
-    await testNewChat(page, chatRequest);
+    await testNewChat(page, chatRequest, true);
   });
 
   test('From Keyboard Shortcut', async ({ page, baseURL }) => {
@@ -33,39 +32,11 @@ test.describe('Chat', () => {
       await page.keyboard.insertText(QUESTION);
 
       // https://playwright.dev/docs/events#waiting-for-event
-      const chatRequestPromise = page.waitForRequest(request => request.url() === `${baseURL}/api/v1/chats` && request.method() === 'POST');
+      const chatRequestPromise = getChatRequestPromise(page, baseURL);
       await page.keyboard.press('ControlOrMeta+Enter');
       return await chatRequestPromise;
     });
 
-    await testNewChat(page, chatRequest);
+    await testNewChat(page, chatRequest, true);
   });
 });
-
-async function testNewChat (page: Page, chatRequest: Request) {
-  await test.step('Wait page changes', async () => {
-    await page.waitForURL(/\/c\/.+/);
-
-    expect(await page.title()).toContain(QUESTION);
-    await page.getByRole('heading', { name: QUESTION }).waitFor({ state: 'visible' });
-  });
-
-  const streamText = await test.step('Wait for chat stop', async () => {
-    const chatResponse = await chatRequest.response();
-    expect(chatResponse.ok()).toBe(true);
-
-    // Feedback button indicates chat ends.
-    await page.locator('button', { has: page.locator('svg.lucide-message-square-plus') }).waitFor({ state: 'visible' });
-
-    return await chatResponse.text();
-  });
-
-  await test.step('Check response text', async () => {
-    const lastLine = streamText.split('\n').filter(t => !!t.trim()).slice(-1)[0];
-    expect(lastLine).toMatch(/^2:/);
-    const message = JSON.parse(lastLine.slice(2))[0].assistant_message;
-
-    expect(message.finished_at).toBeTruthy();
-    expect(message.content.trim().length).toBeGreaterThan(0);
-  });
-}
