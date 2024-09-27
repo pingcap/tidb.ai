1d7d01f861bcea25ece0e3dcea1252f7cf871be7
IANTHEREAL <ArGregoryIan@gmail.com>
2024-09-27 17:09:23 +0800

    remove useless code

--- backend/app/rag/chat.py
+++ backend/app/rag/chat.py
@@ -43,7 +43,6 @@ from app.rag.knowledge_graph.graph_store import (
 )
 from app.rag.knowledge_graph import KnowledgeGraphIndex
 from app.rag.chat_config import ChatEngineConfig, get_default_embedding_model
-from app.rag.router import QueryRouter
 from app.rag.types import (
     MyCBEventType,
     ChatMessageSate,
