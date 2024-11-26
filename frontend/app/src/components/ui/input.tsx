82aecd1ba3335b8b06fa709db4e7413a458b158c
Jagger <634750802@qq.com>
2024-11-26 19:31:25 +0800

    fix type

--- frontend/app/src/components/ui/input.tsx
+++ frontend/app/src/components/ui/input.tsx
@@ -2,7 +2,10 @@ import * as React from "react"
 
 import { cn } from "@/lib/utils"
 
-const Input = React.forwardRef<HTMLInputElement, React.ComponentProps<"input">>(
+export interface InputProps extends React.ComponentProps<"input"> {
+}
+
+const Input = React.forwardRef<HTMLInputElement, InputProps>(
   ({ className, type, ...props }, ref) => {
     return (
       <input
