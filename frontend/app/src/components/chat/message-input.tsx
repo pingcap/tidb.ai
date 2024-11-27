ab41ecd0636328358ff433efeb4bd797682e096e
Jagger <634750802@qq.com>
2024-11-27 13:11:36 +0800

    fix chat engine select

--- frontend/app/src/components/chat/message-input.tsx
+++ frontend/app/src/components/chat/message-input.tsx
@@ -71,7 +71,7 @@ export function MessageInput ({
               <SelectItem key={item.name} value={String(item.name)} textValue={item.name}>
               <span className="flex items-center gap-2">
                 {item.is_default ? <Badge variant="outline" className="text-green-500 border-green-500/50">default</Badge> : item.name}
-                {item.engine_options.external_engine_config
+                {!!item.engine_options.external_engine_config?.stream_chat_api_url
                   ? <Badge>External Engine (StackVM)</Badge>
                   : item.engine_options.knowledge_graph?.enabled !== false /* TODO: require default config */
                     ? <Badge variant="secondary">Knowledge graph enabled</Badge>
