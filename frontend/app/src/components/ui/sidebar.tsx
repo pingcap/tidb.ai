49e210fca6be46a32d7c9057f3903599a0c30fb5
Mini256 <minianter@foxmail.com>
2024-12-03 17:02:28 +0800

    fix: revert auto import sort

--- frontend/app/src/components/ui/sidebar.tsx
+++ frontend/app/src/components/ui/sidebar.tsx
@@ -1,24 +1,23 @@
 "use client"
 
 import * as React from "react"
+import { Slot } from "@radix-ui/react-slot"
+import { VariantProps, cva } from "class-variance-authority"
+import { PanelLeft } from "lucide-react"
 
+import { useIsMobile } from "@/hooks/use-mobile"
+import { cn } from "@/lib/utils"
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+import { Separator } from "@/components/ui/separator"
 import { Sheet, SheetContent } from "@/components/ui/sheet"
+import { Skeleton } from "@/components/ui/skeleton"
 import {
   Tooltip,
   TooltipContent,
   TooltipProvider,
   TooltipTrigger,
 } from "@/components/ui/tooltip"
-import { VariantProps, cva } from "class-variance-authority"
-
-import { Button } from "@/components/ui/button"
-import { Input } from "@/components/ui/input"
-import { PanelLeft } from "lucide-react"
-import { Separator } from "@/components/ui/separator"
-import { Skeleton } from "@/components/ui/skeleton"
-import { Slot } from "@radix-ui/react-slot"
-import { cn } from "@/lib/utils"
-import { useIsMobile } from "@/hooks/use-mobile"
 
 const SIDEBAR_COOKIE_NAME = "sidebar:state"
 const SIDEBAR_COOKIE_MAX_AGE = 60 * 60 * 24 * 7
