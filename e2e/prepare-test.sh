bd71974e9627524ba2e74637541fca1065f5c07b
Jagger <634750802@qq.com>
2024-12-10 10:01:50 +0800

    fix

--- e2e/prepare-test.sh
+++ e2e/prepare-test.sh
@@ -5,7 +5,7 @@ set -e
 # Download tiup
 function prepare_tidb () {
   curl --proto '=https' --tlsv1.2 -sSf https://tiup-mirrors.pingcap.com/install.sh | sh
-  tiup install tidb:v8.4.0 pd tidb tikv tiflash
+  /home/runner/.tiup/bin/tiup install tidb:v8.4.0 pd tidb tikv tiflash
 }
 
 # Pull docker images
