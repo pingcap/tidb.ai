0ef3888f13148f384e54571a85ef69af7a1aac94
Jagger <634750802@qq.com>
2024-12-10 10:25:06 +0800

    update ci

--- e2e/prepare-test.sh
+++ e2e/prepare-test.sh
@@ -7,7 +7,7 @@ TIDB_VERSION=v8.4.0
 # Download tiup
 function prepare_tidb () {
   curl --proto '=https' --tlsv1.2 -sSf https://tiup-mirrors.pingcap.com/install.sh | sh
-  PATH=${PATH}:/home/runner/.tiup/bin/tiup
+  PATH=${PATH}:/home/runner/.tiup/bin
   tiup install playground tidb:${TIDB_VERSION} pd:${TIDB_VERSION} tikv:${TIDB_VERSION} tiflash:${TIDB_VERSION}
 }
 
